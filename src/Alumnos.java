76e443f373a4f1a65ad687c291bd493f25a2c399
Suri Vazquez <suri.sinai@gmail.com>
2023-03-25 10:19:36 -0600

    examen

---
+++ src/Alumnos.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alumnos {
+    public static void main(String[] args) {
+        List Alumnos = new ArrayList();
+        Alumnos.add("Juan");
+        Alumnos.add("leonardo");
+        Alumnos.add("Patricia");
+        Alumnos.add("Diego");
+        Alumnos.add("Elizbeth");
+        Alumnos.add("Maria");
+        Alumnos.add("Rosa");
+        Alumnos.add("Luis");
+        Alumnos.add("Mariel");
+        Alumnos.add("Enrique");
+
+
+
+
+        System.out.println(Alumnos);
+
+    }
+}
